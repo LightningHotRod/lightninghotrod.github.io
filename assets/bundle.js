5ca50e3e938524e7194f6e1b22cea10db9c9d69c
LightningHotRod <126213962+LightningHotRod@users.noreply.github.com>
2023-02-25 18:01:45 -0700

    Delete assets directory

--- assets/bundle.js
+++
@@ -1,8 +0,0 @@
-async function copyScriptToClipboard(scripturl) {
-	const scriptData = await (await fetch(scripturl)).text();
-	navigator.clipboard.writeText(scriptData).then(function () {
-		alert("Copied script to clipboard.");
-	}, function (err) {
-		alert('Error copying script.');
-	});
-};
